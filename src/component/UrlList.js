3e9bb5bac4c8ac479d8cfc32d91d9955b3c34c7d
Shiv Kumar <shiv@poshmark.com>
2021-06-14 07:50:51 +0530

    add url-list component

---
+++ src/component/UrlList.js
@@ -0,0 +1,54 @@
+import React, {useEffect,useState} from 'react'
+import { Link } from 'react-router-dom';
+
+const UrlList = () => {
+  const [urls, setUrls] = useState(null);
+  const makeApiCall = async (url,method) => {
+     const response = await fetch(url, {
+      method: method,
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    return response.json(); 
+  }
+
+  const getAllUrls = async () => {
+    const data = await makeApiCall('http://localhost:3000/api/allUrl', 'GET');
+    setUrls(data.urls)
+  }
+
+  useEffect(() => {
+    getAllUrls()
+  }, [])
+
+  return (
+    <div>
+      <h1> Url Shortner</h1>
+      <div className="url-list--info">
+        <h3>All Urls</h3>
+        <Link to="/">Shorten New Url</Link>
+      </div>
+      <table>
+        <thead >
+          <th>Long Url</th>
+          <th>Short Url</th>
+          <th>Expiry Date</th>
+        </thead>
+      {
+        urls && urls.map((url)=> {
+          return (
+              <tr key={url.shortUrlId}>
+                <td> <a href={url.longUrl}> {url.longUrl} </a> </td>
+                <td> <a href={url.shortUrl}> {url.shortUrl} </a>  </td>
+                <td> {url.expiryDate} </td>
+              </tr>
+          )
+        })
+      }
+       </table>
+    </div>
+  )
+}
+
+export default UrlList
